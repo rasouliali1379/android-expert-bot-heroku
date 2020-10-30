aa407347ff444eccba4a13403c2be3d7a9359124
Ali Rasouli <samrd200046@gmail.com>
2020-10-30 11:19:50 +0330

    8th commit

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
 import asyncio
-from aiogram import Bot, Dispatcher, executor
+import logging
+from aiogram import Bot
+from aiogram.dispatcher import Dispatcher
 from aiogram.utils.executor import start_webhook
 
-BOT_TOKEN = os.getenv("TOKEN")
-WEBHOOK_HOST = "https://android-expert-bot.herokuapp.com/"
-WEBHOOK_PATH = 'webhook/'
+BOT_TOKEN = os.environ.get('TOKEN')
+WEBHOOK_HOST = "https://android-expert-bot.herokuapp.com"
+WEBHOOK_PATH = '/webhook'
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
 WEBAPP_HOST = '0.0.0.0'
@@ -14,15 +16,25 @@ WEBAPP_PORT = os.environ.get('PORT')
 bot = Bot(BOT_TOKEN, parse_mode="HTML")
 dp = Dispatcher(bot)
 
-async def on_startup(app):
+async def on_startup(dp):
     await bot.set_webhook(WEBHOOK_URL)
 
 async def on_shutdown(dp):
-    pass
+    logging.warning('Shutting down..')
+    await bot.delete_webhook()
+    await dp.storage.close()
+    await dp.storage.wait_closed()
+
+    logging.warning('Bye!')
+
 
 if __name__ == '__main__':
     from handlers import dp
 
-    start_webhook(dispatcher=dp, webhook_path=WEBHOOK_PATH,
-                  on_startup=on_startup, on_shutdown=on_shutdown,
-                  host=WEBAPP_HOST, port=WEBAPP_PORT)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT)
